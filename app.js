c84904fbeb928044f8b5535c6a68ce5c443253fc
faizan dhedhi <120604695+muhammad-faizan-mohsin@users.noreply.github.com>
2023-03-21 19:22:44 +0500

    MATH METHODS assignment compeleted

---
+++ app.js
@@ -0,0 +1,80 @@
+// Answer # 1
+
+// var first =  prompt("enter first name");
+// var second = prompt("enter last name");
+
+// var fullName = first+" "+second;
+
+// console.log(fullName)
+
+
+//  Answer # 2
+
+// var favMobilePhone = prompt("enter Favorite Mobile Phone")
+// var length = favMobilePhone.length;
+// console.log(length)
+
+//  Answer # 3
+
+// var first = "Pakistani";
+
+// var second = first.indexOf("n")
+
+// console.log(second)
+ 
+
+//  Answer # 4
+
+// var first = "Hello World";
+
+// var second = first.lastIndexOf("l")
+
+// console.log(second)
+
+
+//  Answer # 5
+// var last = "Pakistani";
+
+// var lindex = last.charAt(3)
+
+// console.log(lindex)
+
+// Answer # 6
+// var first = "faizan";
+// var second ="mohsin";
+
+// var fullName = first.concat(" ",second);
+
+// console.log(fullName)
+
+// Answer # 7
+
+// var a ="Hyderabad"
+// var res = a.replace("Hyder","islam")
+// console.log(res)
+
+// Answer # 8
+// var a = +"472";    
+
+// console.log(a)
+
+
+// Answer # 9
+// var a = prompt("enter value");
+// var b = a.toUpperCase();
+
+// console.log(b)
+
+// Answer # 10
+
+// var a = prompt("enter value");
+// var b = a[0].toUpperCase()
+// console.log(b+a.slice(1))
+
+
+// Answer # 11
+
+    // var num = 35.36;
+    // var a = num.replace("." , " ")
+
+    console.log(a)
